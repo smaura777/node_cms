6e79d4dd03ff6ed1f252f84811f1ae649ee2af29
Samuel Maura <smaura777@gmail.com>
2013-05-02 23:41:08 -0400

    Adding request handler and routing

---
+++ requestHandlers.js
@@ -0,0 +1,12 @@
+
+function start(){
+	console.log("Request handler 'start' called. ");
+}
+
+function upload(){
+	console.log("Request handler 'upload' called. ");
+}
+
+exports.start = start;
+exports.upload = upload;
+
