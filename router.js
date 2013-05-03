ac8abf3cff03cabe62683ee4dcef48accea65bfa
Samuel Maura <smaura777@gmail.com>
2013-05-02 23:41:47 -0400

    Adding request handler and routing

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function route(pathname) {
-  console.log("About to route a request for " + pathname);
+function route(handle,pathname) {
+	if ( typeof handle[pathname] === 'function'){
+		console.log("About to route a request for " + pathname);
+		handle[pathname]();
+	}
+	else {
+		console.log("No route for " + pathname);
+	}
+  
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 var http = require("http");
 var url = require("url");
-function start(){
-http.createServer(function(req,resp){
-    //console.log(req.headers); 
-    var pathname = url.parse(req.url).pathname;
-    console.log("Request for "+pathname+" received");
-	resp.writeHead(200,{"Content-Type":"text/html"});
-	resp.write("<!DOCTYPE html><html><head><title>Node</title></head>"+
-	"<body><div style='width:500px;margin:50px auto 0 auto; background-color:#fff;outline:1px solid #000; ' >");
-	resp.write("<div><p>Hello Node.js World</p></div>");
-	resp.write("<div><p>Received request from "+req.headers['user-agent']+"</p></div>");
-	resp.write("</div></body></html>");
-	resp.end();
-}).listen(8084,function(){
-console.log("Connection receive for note service ");
-});
 
+function start(route,handle){
+	http.createServer(function(req,resp){
+    	//console.log(req.headers); 
+    	var pathname = url.parse(req.url).pathname;
+    	route(handle,pathname);
+    	console.log("Request for "+pathname+" received");
+		resp.writeHead(200,{"Content-Type":"text/html"});
+		resp.write("<!DOCTYPE html><html><head><title>Node</title></head>"+
+		"<body><div style='width:500px;margin:50px auto 0 auto; background-color:#fff;outline:1px solid #000; ' >");
+		resp.write("<div><p>Hello Node.js World</p></div>");
+		resp.write("<div><p>Received request from "+req.headers['user-agent']+"</p></div>");
+		resp.write("</div></body></html>");
+		resp.end();
+	}).listen(8084,function(){
+		  console.log("Connection receive for note service ");
+	});
+       
 }
 
 exports.start =  start;
