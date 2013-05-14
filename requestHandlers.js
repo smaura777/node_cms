e0beb90f2b4ae8d2acda423213b1a22e30c9b60c
Samuel Maura <smaura777@gmail.com>
2013-05-14 16:01:33 -0400

    latest

--- requestHandlers.js
+++ requestHandlers.js
@@ -22,10 +22,25 @@ function _default(resp){
 }
 
 
-function pageStart(resp){
+function pageHeaderStart(resp){
 	resp.write("<!DOCTYPE html><html lang=\"en\"><head><title>Bootstrap 101 Template</title>\
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
-     </head><body>");
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+}
+
+function pageHeaderEnd(resp){
+	resp.write("</head><body>");
+}
+
+function pageInlineCSS(resp){
+	resp.write("<style type=\"text/css\">\
+	    body {font-size:12px; font-family:\"Helvetica Neue\", arial, sans-serif;}\
+	    .main {margin-left:20px; margin-right:20px; padding-left:10px; border:2px solid #ededed; min-height:500px;}\
+		#main_menu {width:300px; margin-left:0px;}\
+		#main_menu ul.menu_set {padding:0; margin:0;}\
+		#main_menu ul.menu_set li {display:inline; margin-right:5px; padding:1px;}\
+		#main_menu ul.menu_set li span {font-weight:bold; font-size:1.2em;}\
+		.comment_form {margin-top: 60px; width:400px;}\
+	</style>");	
 }
 
 function pageEnd(resp){
@@ -69,16 +84,20 @@ function start(resp,postData){
      }
      
      resp.writeHead(200,{"Content-Type":"text/html"});
-     pageStart(resp);
-   
+     pageHeaderStart(resp);
+     pageInlineCSS(resp);
+     pageHeaderEnd(resp);
     
 
-       resp.write("<div class=\"main\"><h2> Comments count : "+counter+" </h2><div class=\"comments\"></div>\
-     			    <div class=\"result\">"+my_data+"</div>\
+       resp.write("<div class=\"main\">\
+       <div id=\"main_menu\">\
+       		<ul class=\"menu_set\"> <li><span>home</span></li> <li><span>upload</span></li> <li><span>misc</span></li> </ul>\
+       </div>\
      			    <div class=\"comment_form\">\
-     			    	<form action=\"/start\" method=\"POST\">\
-     			    		<textarea name=\"comment_data\" rows=\"20\" cols=\"60\"></textarea>\
-     			    		<input type=\"submit\" value=\"status\">\
+     			    	<form action=\"/upload\" method=\"POST\" enctype=\"multipart/form-data\">\
+     			    		<div><textarea placeholder=\"What\'s up? \" name=\"comment_data\" rows=\"5\" cols=\"60\"></textarea></div>\
+     			    		<div><input type=\"file\" name=\"media_asset\" accept=\"image/gif, image/jpeg, image/psd, application/zip, image/tiff, application/pdf, image/png\"></div>\
+     			    		<div><input type=\"submit\" value=\"post\"></div>\
      			    	</form>\
      			   </div>\
      		     </div>");
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require("http");
 var url = require("url");
+var formidable = require("formidable");
+var sys = require("sys");
+
 
 function start(route,handle){
 	http.createServer(function(req,resp){
@@ -8,15 +11,43 @@ function start(route,handle){
     	var postData = "";
     	req.setEncoding("utf8");
     	
+    	if (pathname =='/upload' && req.method.toLowerCase() == 'post'){
+    		console.log("Uploading ...");
+    		
+    		//req.addListener("end",function(chunk){
+    		  console.log("End post");
+			  var form = new formidable.IncomingForm();
+			  form.parse(req,function(err,fields,files){
+				resp.writeHead(200,{'Content-type':'text/plain'});
+				resp.write('received upload: \n\n');
+				resp.end(sys.inspect({fields:fields,files:files}));
+			
+			    });
+    	    
+    	    //});
+    		
+    		return;	
+    	}
+    	else if (req.method.toLowerCase() != 'post'){
+    		route(handle,pathname,resp,postData);
+    	}
+    	
+    	/**
     	req.addListener("data",function(chunk){
     		postData += chunk;
     		console.log("Received data chunk");
     	});
     	
+    
+    	
     	req.addListener("end",function(chunk){
     		console.log("Done receiving");
     		route(handle,pathname,resp,postData);
     	});
+    	
+    	
+        **/
+        	
     	
     	//route(handle,pathname,resp);
     	console.log("Request for "+pathname+" received");
